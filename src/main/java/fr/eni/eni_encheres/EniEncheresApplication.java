85e530c31abce8be490433de137ac95deb783c39
548access45 <86810217+548access45@users.noreply.github.com>
2025-01-27 12:18:39 +0100

    Création des fichiers et dépendances

---
+++ src/main/java/fr/eni/eni_encheres/EniEncheresApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.eni_encheres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EniEncheresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EniEncheresApplication.class, args);
+    }
+
+}

6888281cbda4b50e283f9b183fde1a9ed59b4495
548access45 <86810217+548access45@users.noreply.github.com>
2025-01-27 12:18:39 +0100

    Création des fichiers et dépendances

---
+++ src/test/java/fr/eni/eni_encheres/EniEncheresApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.eni_encheres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EniEncheresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

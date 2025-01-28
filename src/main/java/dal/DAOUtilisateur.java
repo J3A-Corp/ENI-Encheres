165277a7d565499a499ec77a9c66ab21410fac89
Arnogorn <arnaud.plevin@gmail.com>
2025-01-28 11:23:33 +0100

    AjoutUtilisateurDAO
    crud sur l'utilisateur

--- src/main/java/dal/DAOUtilisateur.java
+++ src/main/java/dal/DAOUtilisateur.java
@@ -1,4 +1,26 @@
 package dal;
 
+import bo.Utilisateurs;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+
 public interface DAOUtilisateur {
+
+
+    int create(Utilisateurs utilisateurs);
+
+    List<Utilisateurs> read();
+
+    Utilisateurs read(int id);
+
+    void update(Utilisateurs utilisateurs);
+
+    void delete(Utilisateurs utilisateurs);
+
+    void delete(int noUtilisateur);
 }
